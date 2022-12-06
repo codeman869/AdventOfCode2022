b5c09e6e3705571ab7484970c6f1bf5433731403
Cody Deckard <cjdeckard@gmail.com>
2022-12-05 18:51:56 -0800

    Day 5 code updates

--- Day 5/main.c
+++ Day 5/main.c
@@ -2,6 +2,8 @@
 #include <string.h>
 #include <stdlib.h>
 
+#define MAX_LINE 50
+
 void read_cargo_line(const char* line, size_t len, char* cols[]);
 char* move_cargo(char* from,char* to);
 char* movencargo(char* from, char* to, int len);
@@ -10,10 +12,10 @@ int main(void) {
     FILE *fp;
     char* cargo_line; 
     // Allocate space for reading the file line by line
-    cargo_line = (char*) malloc(50 * sizeof(char)); 
+    cargo_line = (char*) malloc(MAX_LINE * sizeof(char)); 
 
     fp = fopen("cargo.txt", "r");
-    fgets(cargo_line,50,fp);
+    fgets(cargo_line,MAX_LINE,fp);
 
     // Determine number of cargo spaces and allocate space for them
     int num_columns = strlen(cargo_line)/4;
@@ -21,14 +23,14 @@ int main(void) {
     char *cols2[num_columns];
 
     for(int i=0;i<num_columns;i++) {
-        cols[i] = (char*)malloc(50*sizeof(char));
-        cols2[i] = (char*)malloc(50*sizeof(char));
+        cols[i] = (char*)malloc(MAX_LINE*sizeof(char));
+        cols2[i] = (char*)malloc(MAX_LINE*sizeof(char));
     }
 
     read_cargo_line(cargo_line,strlen(cargo_line),cols);
     read_cargo_line(cargo_line,strlen(cargo_line),cols2);
 
-    while(fgets(cargo_line,50,fp) != NULL) {
+    while(fgets(cargo_line,MAX_LINE,fp) != NULL) {
 
         if(strstr(cargo_line," 1 ") != NULL) {
             break;
@@ -41,19 +43,23 @@ int main(void) {
     //Process instructions:
     int quantity,from,to;
 
-    fgets(cargo_line,50,fp);
+    fgets(cargo_line,MAX_LINE,fp);
 
-    while(fgets(cargo_line,50,fp) != NULL) {
+    while(fgets(cargo_line,MAX_LINE,fp) != NULL) {
 
         sscanf(cargo_line,"move %d from %d to %d", &quantity,&from,&to);
         //Part 1 moves
         for(int i=0;i<quantity;i++) {
 
-            cols[to-1] = move_cargo(cols[from-1],cols[to-1]);
+            char *tmp = move_cargo(cols[from-1],cols[to-1]);
+            free(cols[to-1]);
+            cols[to-1] = tmp;
         }
 
         //Part 2 moves
-        cols2[to-1] = movencargo(cols2[from-1],cols2[to-1],quantity);
+        char *tmp2 = movencargo(cols2[from-1],cols2[to-1],quantity);
+        free(cols2[to-1]);
+        cols2[to-1] = tmp2;
 
     }
     //Print output/order
@@ -62,13 +68,14 @@ int main(void) {
         printf("%s\n",cols[i]);
     }
 
+    //Print output 2
     printf("----\n");
     for(int i=0;i<num_columns;i++) {
         printf("%s\n",cols2[i]);
     }
 
-
     //clean up
+    fclose(fp);
     free(cargo_line);
     for(int i=0;i<num_columns;i++) {
         free(cols[i]);
@@ -92,8 +99,8 @@ void read_cargo_line(const char* line, size_t len, char* cols[]) {
 
 char* move_cargo(char* from,char* to) {
 
-    char* output = (char*) malloc(50*sizeof(char));
-    strncpy(output,from,1);
+    char* output = (char*) malloc(MAX_LINE*sizeof(char));
+    memmove(output,from,1);
     strcat(output,to);
 
     memmove(from,from+1,strlen(from));
@@ -103,8 +110,8 @@ char* move_cargo(char* from,char* to) {
 
 char* movencargo(char* from, char* to, int len) {
 
-    char* output = (char*) malloc(50*sizeof(char));
-    strncpy(output,from,len);
+    char* output = (char*) malloc(MAX_LINE*sizeof(char));
+    memmove(output,from,len);
     strcat(output,to);
 
     memmove(from,from+len,strlen(from));
